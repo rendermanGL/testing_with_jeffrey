4aae4fc05d593e19e1323efc894edabcb4dea42e
Jeffrey <martin.jeffrey44@gmail.com>
2026-05-03 19:47:08 +0400

    I made a change
    Find the change I've made

---
+++ calc_function.py
@@ -0,0 +1,16 @@
+def calculate_distance():
+    planet1 = input("Enter the name of the first planet: ").strip()
+    planet2 = input("Enter the name of the second planet: ").strip()
+
+    try:
+        distance = float(input(f"Enter the distance between {planet1} and {planet2} in million kilometers: "))
+    except ValueError:
+        print("Invalid distance. Please enter a number.")
+        return None
+
+    print(f"The distance between {planet1} and {planet2} is {distance} million kilometers.")
+    return disnsdfsdgsfdgsdfg
+
+
+if __name__ == "__main__":
+    calculate_distance()
